40fc687466218143929c1b4bea957c53e9b467a5
deiu <andrei@fcns.eu>
2015-06-17 18:38:24 -0400

    Added filtering and checkboxes

--- app/app.js
+++ app/app.js
@@ -12,13 +12,15 @@ Contacts.controller('Main', function($scope) {
         {
             name: "First contact",
             email: "first@email.com",
-            bookmarked: false
+            favorite: '',
+            checked: false
         },
         {
             name: "Second contact",
             email: "second@example.org",
-            phone: "+1-231-1134-12312",
-            bookmarked: true
+            phone: "+1-231-114-1231",
+            favorite: 'favorite',
+            checked: false
         }
     ];
 
@@ -32,6 +34,28 @@ Contacts.controller('Main', function($scope) {
         $scope.loggedIn = false;
     };
 
+    $scope.hoverContact = function(i, hover) {
+        if ($scope.contacts) {
+            if ($scope.contacts[i].checked) {
+                $scope.contacts[i].showcheckbox = true;
+                $scope.contacts[i].hidepic = true;
+                console.log("Checkbox checked");
+            } else {
+                console.log("Checkbox not checked");
+                if (hover) {
+                    console.log("Hovering");
+                    $scope.contacts[i].showcheckbox = true;
+                    $scope.contacts[i].hidepic = true;
+                } else {
+                    console.log("No hovering");
+                    $scope.contacts[i].showcheckbox = false;
+                    $scope.contacts[i].hidepic = false;
+                }
+            }
+            console.log("checked: " + $scope.contacts[i].checked + " / showcheckbox: "+$scope.contacts[i].showcheckbox + " / hidepic: " + $scope.contacts[i].hidepic + "\n--------");
+            // $scope.contacts[i].showpic = ($scope.contacts[i].checked && !hover)?false:true;
+        }
+    };
 
     
 });
